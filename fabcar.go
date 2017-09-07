07004a438415f03c651313dbebf87eaff6d9147c
gportillo <gportillo94@gmail.com>
2017-09-07 13:26:06 -0500

    chaincode polizasPorAseguradora

--- fabcar.go
+++ fabcar.go
@@ -120,6 +120,59 @@ func (s *SmartContract) polizaPorId(APIstub shim.ChaincodeStubInterface, args []
 	return shim.Success(carAsBytes)
 }
 
+func (s *SmartContract) polizasPorAseguradora(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+
+	if len(args) != 1 {
+		return shim.Error("Incorrect number of arguments. Expecting 1")
+	}
+
+	startKey := "0"
+	endKey := "999"
+
+	resultsIterator, err := APIstub.GetStateByRange(startKey, endKey)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	defer resultsIterator.Close()
+
+	// buffer is a JSON array containing QueryResults
+	var buffer bytes.Buffer
+	var pol Poliza
+	buffer.WriteString("[")
+
+	bArrayMemberAlreadyWritten := false
+
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return shim.Error(err.Error())
+		}
+		json.Unmarshal(queryResponse.Value, &pol)
+		if pol.Aseguradora.Nombre == args[0] {
+			// Add a comma before array members, suppress it for the first array member
+			if bArrayMemberAlreadyWritten == true {
+				buffer.WriteString(",")
+			}
+			buffer.WriteString("{\"Key\":")
+			buffer.WriteString("\"")
+			buffer.WriteString(queryResponse.Key)
+			buffer.WriteString("\"")
+
+			buffer.WriteString(", \"Record\":")
+			// Record is a JSON object, so we write as-is
+			buffer.WriteString(string(queryResponse.Value))
+			buffer.WriteString("}")
+			bArrayMemberAlreadyWritten = true
+		}
+	}
+
+	buffer.WriteString("]")
+
+	fmt.Printf("- queryAllCars:\n%s\n", buffer.String())
+
+	return shim.Success(buffer.Bytes())
+}
+
 func (s *SmartContract) todasPolizas(APIstub shim.ChaincodeStubInterface) sc.Response {
 
 	startKey := "0"
@@ -215,6 +268,8 @@ func (s *SmartContract) Invoke(APIstub shim.ChaincodeStubInterface) sc.Response 
 		return s.todasPolizas(APIstub)
 	} else if function == "changeInfoPoliza" {
 		return s.changeInfoPoliza(APIstub, args)
+	} else if function == "polizasPorAseguradora" {
+		return s.polizasPorAseguradora(APIstub, args)
 	}
 
 	return shim.Error("Invalid Smart Contract function name.")
