27f30650d917292f5f1688ee853bf454a5146405
gportillo <gportillo94@gmail.com>
2017-09-08 12:48:41 -0500

    comentarios app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser  = require("body-parser");
 const app = express(); 
 var query = require("./query.js");
 var invoke = require("./invoke.js");
+var ID = 3; 
 
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true })); 
@@ -16,6 +17,32 @@ app.get("/", function(req, res){
 });
 
 app.get("/todasPolizas", function(req, res){
+	/*
+	Entrada: Nada
+	Salida: Un arreglo de objetos, por ej.:
+		[
+			{"Key":"1", "Record":{
+				"aseguradora":{"idAseguradora":"aseg01","nombre":"AXXA"},
+				"automovil":{"placa":"ABC123","vin":"h0l4mund0"},
+				"cliente":{"apellidoMaterno":"flores","apellidoPaterno":"portillo","correo":"","nombre":"guillermo","telefono":""},
+				"estatus":false,
+				"fechaFin":"2016-06-01T00:00:00Z",
+				"fechaIni":"2015-01-30T00:00:00Z",
+				"id":"1",
+				"tipo":0}
+			},
+			{"Key":"2", "Record":{
+				"aseguradora":{"idAseguradora":"aseg01","nombre":"AXXA"},
+				"automovil":{"placa":"DEF456","vin":"h0l4mund0"},
+				"cliente":{"apellidoMaterno":"zxcv","apellidoPaterno":"asdf","correo":"","nombre":"qwer","telefono":""},
+				"estatus":false,
+				"fechaFin":"2016-06-01T00:00:00Z",
+				"fechaIni":"2015-01-30T00:00:00Z",
+				"id":"2",
+				"tipo":0}
+			}
+		]
+	*/
 	var prom = query.query("todasPolizas", []);
 	prom.then(function(polizas){
 		res.send(polizas[0].toString());
@@ -23,6 +50,19 @@ app.get("/todasPolizas", function(req, res){
 });
 
 app.post("/polizaPorId", function (req, res){
+	/*
+	Entrada: Cadena con el ID de la poliza
+	Salida: Un objeto poliza, por ej.:
+	{	"aseguradora":{"idAseguradora":"aseg01","nombre":"AXXA"},
+		"automovil":{"placa":"ABC123","vin":"h0l4mund0"},
+		"cliente":{"apellidoMaterno":"flores","apellidoPaterno":"portillo","correo":"","nombre":"guillermo","telefono":""},
+		"estatus":false,
+		"fechaFin":"2016-06-01T00:00:00Z",
+		"fechaIni":"2015-01-30T00:00:00Z",
+		"id":"1",
+		"tipo":0
+	}
+	*/
 	var prom = query.query("polizaPorId", [req.body.id]);
 	prom.then(function(poliza){
 		res.send(poliza[0].toString());
@@ -30,6 +70,10 @@ app.post("/polizaPorId", function (req, res){
 });
 
 app.post("/polizasPorAseguradora", function (req, res){
+	/*
+	Entrada: Cadena con el nombre de la aseguradora
+	Salida: Ver salida de 'todasPolizas'
+	*/
 	console.log(req.body.aseguradora);
 	var prom = query.query("polizasPorAseguradora", [req.body.aseguradora]);
 	prom.then(function(poliza){
@@ -38,14 +82,38 @@ app.post("/polizasPorAseguradora", function (req, res){
 });
 
 app.post("/createPoliza", function(req, res){
-	var polizaAsString = req.body.create;
-	var prom = invoke.invoke("createPoliza", [polizaAsString])
+	/*
+	Entrada: Una cadena con el siguiente formato JSON. Sin la propiedad id!!!!
+		{	"aseguradora":{"idAseguradora":"","nombre":""},
+			"automovil":{"placa":"","vin":""},
+			"cliente":{"apellidoMaterno":"","apellidoPaterno":"","correo":"","nombre":"","telefono":""},
+			"estatus":false,
+			"fechaFin":"",
+			"fechaIni":"",
+			"tipo":0
+		}
+	*/
+	var poliza = JSON.parse(req.body.create);
+	poliza.id = (ID++).toString();  
+	var prom = invoke.invoke("createPoliza", [JSON.stringify(poliza)]);
 	prom.then(function(transactionID){
 		res.send(transactionID);
 	});
 });
 
 app.post("/changeInfoPoliza", function(req, res){
+	/*
+	Entrada: Una cadena con el siguiente formato JSON.
+		{	"aseguradora":{"idAseguradora":"","nombre":""},
+			"automovil":{"placa":"","vin":""},
+			"cliente":{"apellidoMaterno":"","apellidoPaterno":"","correo":"","nombre":"","telefono":""},
+			"estatus":false,
+			"fechaFin":"",
+			"fechaIni":"",
+			"id":"",
+			"tipo":0
+		}
+	*/
 	var polizaAsString = req.body.change; 
 	var poliza = JSON.parse(polizaAsString);
 	var prom = invoke.invoke("changeInfoPoliza", [poliza.id, polizaAsString]);
